6a849b8fe8874bbdc230769c0bf76e26693d4455
kquinn1998 <kevquinn321@gmail.com>
2019-10-23 13:11:52 +0100

    work on login process

---
+++ HTML/Login.php
@@ -0,0 +1,45 @@
+<?php
+    session_start();
+
+    $servername = "localhost";
+    $username = "root";
+    $dbpassword = "";
+    $dbname = "C00216607_securedev";
+    $count = 0;
+    // Creating a connection
+    $conn = new mysqli($servername, $username, $dbpassword, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        $conn->close();
+        header('location:Login.html.php');
+        die("connection error");
+    }
+    if ( !isset($_POST['username'], $_POST['pass']) ) {
+        // Could not get the data that should have been sent.
+        $conn->close();
+        echo "no details recieved";
+    }
+    if ($stmt = $conn->prepare('SELECT id, pass FROM users WHERE username = ?')) {
+        // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+        $stmt->bind_param('s', $_POST['username']);
+        $stmt->execute();
+        // Store the result so we can check if the account exists in the database.
+        $stmt->store_result();
+    }
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($id, $password);
+        $stmt->fetch();
+        if ($_POST['pass'] == $password) {
+            session_regenerate_id();
+		    $_SESSION['loggedin'] = TRUE;
+		    $_SESSION['name'] = $_POST['username'];
+		    $_SESSION['id'] = $id;
+            header("location:WelcomePage.html.php");
+        } else {
+            header("location:Login.html.php");
+        }
+    } else {
+        header("location:Login.html.php");
+    }
+    $stmt->close();
+?>
--- HTML/WelcomePage.html.php
+++ HTML/WelcomePage.html.php
@@ -1,11 +1,15 @@
 <?php
-    include 'config.php';
+    session_start();
+    if (!isset($_SESSION['loggedin'])) {
+        header('Location: index.html');
+        exit();
+    }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login</title>
+	<title>Create Account</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
@@ -36,50 +40,16 @@
 	<div class="limiter">
 		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
 			<div class="wrap-login100">
-				<form class="login100-form validate-form" action="Login.php" method="POST">
+				<div class="login100-form validate-form" action="CreateAccount.php" method="POST">
 					<span class="login100-form-logo">
 						<i class="zmdi zmdi-landscape"></i>
 					</span>
 
 					<span class="login100-form-title p-b-34 p-t-27">
-						Log in
+						Welcome !
 					</span>
-
-					<div class="wrap-input100 validate-input" data-validate = "Enter username">
-						<input class="input100" type="text" name="username" placeholder="Username">
-						<span class="focus-input100" data-placeholder="&#xf207;"></span>
 					</div>
-
-					<div class="wrap-input100 validate-input" data-validate="Enter password">
-						<input class="input100" type="password" name="pass" placeholder="Password">
-						<span class="focus-input100" data-placeholder="&#xf191;"></span>
-					</div>
-
-					<div class="contact100-form-checkbox">
-						<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-						<label class="label-checkbox100" for="ckb1">
-							Remember me
-						</label>
-					</div>
-
-					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
-							Login
-						</button>
-					</div>
-
-                    <div class="text-center p-t-90">
-						<a class="txt1" href="CreateAccount.html.php">
-							Create Account
-						</a>
-					</div>
-
-					<div class="text-center p-t-20">
-						<a class="txt1" href="#">
-							Forgot Password?
-						</a>
-					</div>
-				</form>
+				</div>
 			</div>
 		</div>
 	</div>
